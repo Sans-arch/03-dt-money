4b695777cb46c7fcb47712e22a0eb17a5f9cfaee
Santiago Negreira <santiago.negreira@gesplan.com.br>
2023-05-04 13:25:11 -0300

    Finish NewTransactionModal component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,15 +1,29 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { CloseButton, Content, Overlay } from "./styles";
+import { X } from "phosphor-react";
 
 export function NewTransactionModal() {
   return (
     <Dialog.Portal>
-      <Dialog.Overlay />
+      <Overlay />
 
-      <Dialog.Content>
+      <Content>
         <Dialog.Title>Nova transação</Dialog.Title>
 
-        <Dialog.Close />
-      </Dialog.Content>
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form action="">
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <button type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </Content>
     </Dialog.Portal>
   )
 }
