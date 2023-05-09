0a424faaa12ea1e5797479ddef7eb377d1817a92
Santiago Negreira <santiago.negreira@gesplan.com.br>
2023-05-09 14:04:51 -0300

    Add React Hook Form, add zod, apply react hook form to all forms

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -28,8 +28,14 @@ export const SearchFormContainer = styled.form`
       border: 1px solid ${props => props.theme["green-300"]};
       color: ${props => props.theme["green-300"]};
       border-radius: 6px;
+      cursor: pointer;
 
-      &:hover {
+      &:disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+
+      &:not(:disabled):hover {
         background: ${props => props.theme["green-500"]};
         border-color: ${props => props.theme["green-300"]};
         color: ${props => props.theme.white};
