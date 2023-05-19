28d543f2216a5e32b2f4aae1a64bfe6c1fdda0d7
Santiago Negreira <santiago.negreira@gesplan.com.br>
2023-05-19 12:11:01 -0300

    Use memo for example in SearchFormComponent to avoid a new render

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -4,8 +4,27 @@ import { useForm } from 'react-hook-form'
 import { useContextSelector } from 'use-context-selector'
 import * as z from 'zod'
 
+import { memo } from 'react'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 import { SearchFormContainer } from './styles'
+
+/**
+ * Por que um componente renderiza?
+ *
+ * - Hooks changed (mudou estado, contexto, reducer);
+ * - Props changed (mudou propriedades)
+ * - Parent rerendered (componente pai renderizou);
+ *
+ * Qual o fluxo de renderização?
+ * 1. O React recria o HTML da interface daquele componente
+ * 2. Compara a versão do HTML recriada com a versão anterior
+ * 3. SE mudou alguma coisa, ele reescreve o HTML na tela
+ *
+ * Memo:
+ * 0. Hooks changed, Props changed (deep comparison)
+ * 0.1: Comparar a versão anterior dos hooks e props
+ * 0.2: SE mudou algo, ele vai permitir a nova renderização.
+ */
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -13,7 +32,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm() {
+function SearchFormComponent() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     (context) => {
@@ -48,3 +67,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
